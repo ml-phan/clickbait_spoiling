b1834d830d46d273a5fdc9ec7cb2670ec1b645a9
René Wolf <rene.wolf.1@uni-potsdam.de>
2023-04-01 12:13:30 +0200

    Replace main.py

--- LSTM character based/main.py
+++ LSTM character based/main.py
@@ -12,6 +12,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 # hyperparameters
+# load data
 data, types = load_data(True)
 test_data, test_types = load_data(False)
 class_list = list(set(types.values()))
@@ -22,24 +23,13 @@ NUM_CLASSES = len(class_list)
 TRAIN_SIZE = len(data)
 
 # parameter model
-HIDDEN_SIZE = 128
+HIDDEN_SIZE = 32
 NUM_LAYERS = 2
 BATCH_SIZE = 1
 
 # parameter training
-NUM_EPOCHS = 1
-# LEARNING_RATE = 0.005
-LEARNING_RATE = 0.01
-
-
-def magic():
-    """ Creates and trains the model. """
-
-    model = LSTM(NUM_SYMBOLS, HIDDEN_SIZE, NUM_LAYERS, NUM_CLASSES, device).to(device)
-    data_generator = data_handler(data, types)
-
-    training(model, BATCH_SIZE, NUM_EPOCHS, LEARNING_RATE, device, TRAIN_SIZE, data_generator)
-    return model
+NUM_EPOCHS = 10
+LEARNING_RATE = 0.005
 
 
 def predict(model, text):
@@ -65,22 +55,25 @@ def evaluation(model):
         correct += 1 if prediction == test_types[key] else 0
         accuracy = 100 * correct/total
 
-        if total % 25 == 0:
-            print(f'Total Samples: {total}; Accuracy: { "{:.2f}".format(accuracy)} %')
+    print(f'Total Samples: {total}; Accuracy: { "{:.2f}".format(accuracy)} %')
+    return accuracy
 
 
-def train_evaluation(model):
-    """ Same as evaluation but uses the training set. """
+def make_model(hidden_size, learning_rate):
+    """ The main function. Takes a hidden_size and learning_rate and creates a model with these. Trains the model on the training data and evaluates the model on the validation data. """
 
-    total = 0
-    correct = 0
+    # create a model
+    model = LSTM(NUM_SYMBOLS, hidden_size, NUM_LAYERS, NUM_CLASSES, device).to(device)
 
-    for key, text in data.items():
-        input_matrix = create_input_matrix(text)
-        prediction = model.predict(input_matrix)
-        total += 1
-        correct += 1 if prediction == types[key] else 0
-        accuracy = 100 * correct/total
+    # train it
+    training(model, BATCH_SIZE, NUM_EPOCHS, learning_rate, device, TRAIN_SIZE, data_handler, data, types)
 
-        if total % 100 == 0:
-            print(f'Total Samples: {total}; Accuracy: { "{:.2f}".format(accuracy)} %')
+    # evaluate it
+    accuracy = evaluation(model)
+
+    return model, accuracy
+
+
+def main():
+    """Returns the model with the best hyperparameters and its accuracy. """
+    return make_model(HIDDEN_SIZE, LEARNING_RATE)
