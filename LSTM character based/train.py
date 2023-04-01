75914c998fa21fef5d1104f01c5de7b79988fb41
René Wolf <rene.wolf.1@uni-potsdam.de>
2023-04-01 12:14:01 +0200

    Replace train.py

--- LSTM character based/train.py
+++ LSTM character based/train.py
@@ -1,4 +1,7 @@
-""" This file contains the training function in order to train the LSTM model.
+"""
+This file contains the training function in order to train the LSTM model.
+This model structure is partly based on the version by Patrick Loeber:
+https://github.com/patrickloeber/pytorch-examples/blob/master/rnn-lstm-gru/main.py
 """
 
 import torch
@@ -6,20 +9,22 @@ import torch.nn as nn
 
 
 # Training Function
-def training(model, batch_size, epochs, learning_rate, device, data_length, data_handler):
+def training(model, batch_size, epochs, learning_rate, device, data_length, data_generator_func, data, types):
 
     # Define learning devices
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-    model.train()
-
+    # Training Loop
     for epoch in range(epochs):
+        model.train()
+        data_generator = data_generator_func(data, types)
+        losses = 0.0
         for n in range(data_length):
             optimizer.zero_grad()
 
             # get data and gold_labels
-            input_matrix, gold_label = next(data_handler)
+            input_matrix, gold_label = next(data_generator)
             input_matrix = input_matrix.to(device)
             gold_label = gold_label.to(device)
 
@@ -31,5 +36,6 @@ def training(model, batch_size, epochs, learning_rate, device, data_length, data
             loss.backward()
             optimizer.step()
 
-            if n % 50 == 0:
-                print(f'Epoch [{epoch+1}/{epochs}], n [{n}], Loss: {loss.item():.4f}')
+            losses += loss.item()
+            if n % 400 == 0:
+                print(f'Epoch [{epoch+1}/{epochs}], Samples [{n}/{data_length}], Loss: {loss.item():.4f}, Loss_avg = {(losses/(n+1)):.4f}')
